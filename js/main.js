d023100e498ab6a28932ff3888974a482c34cbda
AlesDod02 <alessandro.dod@gmail.com>
2023-11-14 15:07:15 +0100

    working

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//chiedo all'utente quanti kilometri vuole percorrere ela sua età
+let name = document.getElementById("Name").value;
+let distance = document.getElementById("Km_da_Percorrere").value;
+let age = document.getElementById("age_select").value;
+
+distance = parseInt(distance);
+let ticketPrice = distance * 0.21;
+if (age == Minorenne) {
+    ticketPrice = ticketPrice * 0.8;
+}
+else if (age == Over65) {
+    ticketPrice = ticketPrice * 0.6
+        ;
+}
+let finalprice = ticketPrice.toFixed(2);
+
+
+function userHasFinishDataCompletion(event) {
+    event.preventDefault();
+
+
+    //calcolo il prezzo del biglietto (0,21 al km)
+    
+    
+
+    // applico lo sconto in base alla età
+    
+
+    
+    document.getElementById("user_price").innerHTML = finalprice;
+}
+
+
+
+
